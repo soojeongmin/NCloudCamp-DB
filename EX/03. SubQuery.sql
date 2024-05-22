c4c5a3300c53c1b5cd40a61958a70bb0538b945b
soojeongmin <soojeongmin@soongsil.ac.kr>
2024-05-22 17:58:29 +0900

    0522

--- EX/03. SubQuery.sql
+++ EX/03. SubQuery.sql
@@ -93,18 +93,88 @@ SELECT E.ENO
 	 , E.ENAME
 	 , E.SAL
 	FROM EMP E
-	E.SAL IN (
+	WHERE E.SAL IN (
 		SELECT SAL
 			FROM EMP
 			WHERE SAL >= 3000
 	); 
 	 
 	 
+-- 1-3. 다중열 서브쿼리
+-- 서브쿼리의 결과가 다중행이면서 다중열인 서브쿼리
+-- FROM, JOIN 절에서만 사용가능
+-- 과목번호, 과목이름, 교수번호, 교수이름을 조회하는 서브쿼리를 작성하여
+-- 기말고사 성적 테이블과 조인하여 과목번호, 과목이름, 교수번호, 교수이름, 기말고사 성적을 조회
+SELECT C.CNO
+	 , C.CNAME
+	 , P.PNO
+	 , P.PNAME
+	 , SC.RESULT
+	FROM COURSE C
+	JOIN SCORE SC
+	ON C.CNO = SC.CNO
+	JOIN PROFESSOR P
+	ON C.PNO = P.PNO;
+
+SELECT A.CNO
+	 , A.CNAME
+	 , A.PNO
+	 , A.PNAME
+	 , SC.RESULT
+	FROM (
+		SELECT C.CNO
+			 , C.CNAME
+			 , P.PNO
+			 , P.PNAME
+			FROM COURSE C
+			JOIN PROFESSOR P
+			ON C.PNO = P.PNO
+		) A
+		JOIN SCORE SC 
+		ON A.CNO = SC.CNO;
+	
+SELECT ST.SNO
+	 , ST.SNAME
+	 , AVG(SC.RESULT)
+	FROM STUDENT ST
+	JOIN SCORE SC
+	ON SC.SNO = ST.SNO
+	GROUP BY ST.SNO, ST.SNAME;
 	 
 	 
-	 
-	 
-	 
-	 
-	 
+-- 학생번호, 학생이름, 과목번호, 과목이름, 기말고사성적, 기말고사 성적 등급, 담당 교수번호, 담당 교수이름 조회하는데
+-- STUDENT, SCORE, SCGRADE 테이블의 내용을 서브쿼리1
+-- COURSE, PROFESSOR 테이블의 내용을 서브쿼리2
+
+SELECT A.SNO
+	 , A.SNAME
+	 , B.CNO
+	 , B.CNAME
+	 , A.RESULT
+	 , A.GRADE
+	 , B.PNO
+	 , B.PNAME
+	FROM (
+		SELECT ST.SNO
+			 , ST.SNAME
+			 , SC.RESULT
+			 , GR.GRADE
+			 , SC.CNO
+			FROM STUDENT ST
+			JOIN SCORE SC
+			ON ST.SNO = SC.SNO
+			JOIN SCGRADE GR
+		  	ON SC.RESULT BETWEEN GR.LOSCORE AND GR.HISCORE
+	) A
+	JOIN (
+		SELECT C.CNO
+			 , C.CNAME
+			 , P.PNO
+			 , P.PNAME
+			FROM COURSE C
+			JOIN PROFESSOR P
+			ON C.PNO = P.PNO
+	
+	) B
+	ON A.CNO = B.CNO;
 					 
